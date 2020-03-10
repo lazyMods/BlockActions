827ce3cc4d23e367fe21a8fc2e30473575606ff4
lazynessmind <lazynessmind@gmail.com>
2020-03-10 12:10:25 +0000

    Implemented Redstone Work

--- src/main/java/com/lazynessmind/blockactions/base/BlockActionBase.java
+++ src/main/java/com/lazynessmind/blockactions/base/BlockActionBase.java
@@ -1,13 +1,17 @@
 package com.lazynessmind.blockactions.base;
 
+import com.lazynessmind.blockactions.api.IRedstoneWork;
+import com.lazynessmind.blockactions.utils.RedstoneWorkRegistry;
 import com.lazynessmind.blockactions.utils.Utils;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.dispenser.ProxyBlockSource;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
@@ -17,14 +21,17 @@ import net.minecraft.util.Mirror;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.loot.LootContext;
 
 import java.util.List;
+import java.util.Random;
 
 public class BlockActionBase extends ModBlock {
 
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    public static final BooleanProperty TRIGGERED = BlockStateProperties.TRIGGERED;
 
     public BlockActionBase(String name) {
         super(name, Block.Properties.from(Blocks.OBSERVER), ItemGroup.REDSTONE);
@@ -32,8 +39,46 @@ public class BlockActionBase extends ModBlock {
     }
 
     @Override
+    public void tick(BlockState state, World worldIn, BlockPos pos, Random random) {
+        this.doWork(worldIn, pos, state);
+    }
+
+    private void doWork(World world, BlockPos pos, BlockState state){
+        ProxyBlockSource proxyblocksource = new ProxyBlockSource(world, pos);
+        BlockActionTileEntity blockTileEntity = proxyblocksource.getBlockTileEntity();
+        IRedstoneWork iRedstoneWork = RedstoneWorkRegistry.REDSTONE_WORKS.get(this);
+        if(iRedstoneWork != null){
+            iRedstoneWork.doWork(blockTileEntity, state, world, pos);
+        }
+    }
+
+    @Override
+    public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+        if(!worldIn.isRemote){
+            if (state.getBlock() != newState.getBlock()) {
+                TileEntity tile = worldIn.getTileEntity(pos);
+                if (tile instanceof BlockActionTileEntity) {
+                    BlockActionTileEntity blockActionTileEntity = (BlockActionTileEntity) tile;
+                    if (!blockActionTileEntity.getUpgradeItems().isEmpty()) {
+                        InventoryHelper.dropItems(worldIn, pos, blockActionTileEntity.getUpgradeItems());
+                    }
+                }
+
+                if (state.hasTileEntity()) {
+                    worldIn.removeTileEntity(pos);
+                }
+            }
+        }
+    }
+
+    @Override
+    public int tickRate(IWorldReader worldIn) {
+        return 60; //3sec
+    }
+
+    @Override
     public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-        return NonNullList.withSize(1, Utils.newStack(asItem()));
+        return NonNullList.withSize(1, Utils.asStack(asItem()));
     }
 
     @Override
@@ -53,22 +98,23 @@ public class BlockActionBase extends ModBlock {
 
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, TRIGGERED);
     }
 
     @Override
-    public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
-        if (state.getBlock() != newState.getBlock()) {
-            TileEntity tile = worldIn.getTileEntity(pos);
-            if (tile instanceof BlockActionTileEntityBase) {
-                BlockActionTileEntityBase blockActionTileEntityBase = (BlockActionTileEntityBase) tile;
-                if (!blockActionTileEntityBase.getUpgradeItems().isEmpty()) {
-                    InventoryHelper.dropItems(worldIn, pos, blockActionTileEntityBase.getUpgradeItems());
+    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
+        ProxyBlockSource proxyBlockSource = new ProxyBlockSource(worldIn, pos);
+        if(proxyBlockSource.getBlockTileEntity() instanceof BlockActionTileEntity){
+            BlockActionTileEntity tileEntity = proxyBlockSource.getBlockTileEntity();
+            if(!tileEntity.isEnergyMode()){
+                boolean isPowered = worldIn.isBlockPowered(pos);
+                boolean isTriggered = state.get(TRIGGERED);
+                if (isPowered && !isTriggered) {
+                    worldIn.getPendingBlockTicks().scheduleTick(pos, this, this.tickRate(worldIn));
+                    worldIn.setBlockState(pos, state.with(TRIGGERED, true), 4);
+                } else if (!isPowered && isTriggered) {
+                    worldIn.setBlockState(pos, state.with(TRIGGERED, false), 4);
                 }
-            }
-
-            if (state.hasTileEntity()) {
-                worldIn.removeTileEntity(pos);
             }
         }
     }
--- src/main/java/com/lazynessmind/blockactions/proxy/CommonProxy.java
+++ src/main/java/com/lazynessmind/blockactions/proxy/CommonProxy.java
@@ -4,6 +4,7 @@ import com.lazynessmind.blockactions.event.BlockRegister;
 import com.lazynessmind.blockactions.event.ItemRegister;
 import com.lazynessmind.blockactions.event.TileEntityRegister;
 import com.lazynessmind.blockactions.net.NetHandler;
+import com.lazynessmind.blockactions.utils.RedstoneWorkRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -22,5 +23,6 @@ public class CommonProxy {
 
     public void setupCommon(FMLCommonSetupEvent event) {
         NetHandler.registerCommon();
+        RedstoneWorkRegistry.initWorks();
     }
 }
