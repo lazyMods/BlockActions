f3881ff99ea919cb0d197d74b71edda26b058c25
lazynessmind <lazynessmind@gmail.com>
2020-03-10 12:08:34 +0000

    Updated work method

--- src/main/java/com/lazynessmind/blockactions/actions/hitaction/HitTileEntity.java
+++ src/main/java/com/lazynessmind/blockactions/actions/hitaction/HitTileEntity.java
@@ -1,7 +1,7 @@
 package com.lazynessmind.blockactions.actions.hitaction;
 
 import com.lazynessmind.blockactions.actions.placeaction.PlacerBlock;
-import com.lazynessmind.blockactions.base.BlockActionTileEntityBase;
+import com.lazynessmind.blockactions.base.BlockActionTileEntity;
 import com.lazynessmind.blockactions.event.TileEntityRegister;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.nbt.CompoundNBT;
@@ -10,12 +10,19 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
 
-public class HitTileEntity extends BlockActionTileEntityBase {
+import javax.annotation.Nonnull;
+
+public class HitTileEntity extends BlockActionTileEntity {
 
     private static final String TAG_DAMAGE = "Damage";
     public float baseDamage = 1f;
 
+    private static final String TAG_ONLY_ADULT = "AttackOnlyAdults";
+    public boolean attackOnlyAdults = false;
+
+    private float dmg;
     private Direction direction;
 
     public HitTileEntity() {
@@ -27,32 +34,18 @@ public class HitTileEntity extends BlockActionTileEntityBase {
         if (this.world != null) {
             if (!this.world.isRemote) {
 
+                this.setFakePlayer(this.world, "HitPlayer");
+
                 if (this.getBlockState().has(PlacerBlock.FACING)) {
                     this.direction = this.getBlockState().get(PlacerBlock.FACING);
                 }
 
-                if (!this.world.isBlockPowered(this.pos)) {
-                    this.workTime--;
-                    this.markDirty();
+                this.decreaseWorkTime();
 
-                    if (this.canWork()) {
-                        if (this.getFacing() != null) {
-                            BlockPos posAtFacing = this.pos.offset(this.getFacing());
-                            AxisAlignedBB area = new AxisAlignedBB(posAtFacing);
-
-                            for (LivingEntity livingEntity : this.world.getEntitiesWithinAABB(LivingEntity.class, area)) {
-                                float dmg = (float) HitUtils.getDamage(this.baseDamage, this.world, posAtFacing).getValue();
-                                if (HitUtils.causeFireDamage(world, posAtFacing)) {
-                                    livingEntity.attackEntityFrom(DamageSource.ON_FIRE, dmg);
-                                    livingEntity.setFire(3);
-                                } else {
-                                    livingEntity.attackEntityFrom(DamageSource.GENERIC, dmg);
-                                }
-                            }
-
-                            this.setWorkTime(this.getCoolDown());
-                            this.markDirty();
-                        }
+                if(this.isEnergyMode()){
+                    this.energyManager.canInsert(true);
+                    if(this.canWork()){
+                        this.doWork(this.world, this.pos, true);
                     }
                 }
             }
@@ -60,15 +53,51 @@ public class HitTileEntity extends BlockActionTileEntityBase {
     }
 
     @Override
+    public void doWork(@Nonnull World world, BlockPos pos, boolean energyMode) {
+        if (this.getFacing() != null) {
+            BlockPos posAtFacing = this.pos.offset(this.getFacing());
+            AxisAlignedBB area = new AxisAlignedBB(posAtFacing);
+            this.dmg = (float) HitUtils.getDamage(this.baseDamage, this.world, posAtFacing).getValue();
+
+            if(this.isEnergyMode() && !this.energyManager.canRemove(this.getEnergyCost())) return;
+
+            for (LivingEntity livingEntity : this.world.getEntitiesWithinAABB(LivingEntity.class, area)) {
+                if (this.attackOnlyAdults) {
+                    if (!livingEntity.isChild()) {
+                        this.attackEntities(posAtFacing, livingEntity, dmg);
+                        HitUtils.damageItem(world, posAtFacing, this.getFakePlayer());
+                    }
+                } else {
+                    this.attackEntities(posAtFacing, livingEntity, dmg);
+                    HitUtils.damageItem(world, posAtFacing, this.getFakePlayer());
+                }
+                if(energyMode) this.energyManager.removeEnergy(this.getEnergyCost());
+            }
+            this.setWorkTime(this.getCoolDown());
+        }
+    }
+
+    private void attackEntities(BlockPos posAtFacing, LivingEntity livingEntity, float dmg) {
+        if (HitUtils.causeFireDamage(world, posAtFacing)) {
+            livingEntity.attackEntityFrom(DamageSource.ON_FIRE, dmg);
+            livingEntity.setFire(3);
+        } else {
+            livingEntity.attackEntityFrom(DamageSource.GENERIC, dmg);
+        }
+    }
+
+    @Override
     public void read(CompoundNBT compound) {
         super.read(compound);
         this.baseDamage = compound.getFloat(TAG_DAMAGE);
+        this.attackOnlyAdults = compound.getBoolean(TAG_ONLY_ADULT);
     }
 
     @Override
     public CompoundNBT write(CompoundNBT compound) {
         CompoundNBT nbt = super.write(compound);
         nbt.putFloat(TAG_DAMAGE, this.baseDamage);
+        nbt.putBoolean(TAG_ONLY_ADULT, this.attackOnlyAdults);
         return nbt;
     }
 
@@ -77,9 +106,12 @@ public class HitTileEntity extends BlockActionTileEntityBase {
     }
 
     @Override
-    public CompoundNBT getLines() {
-        CompoundNBT nbt = super.getLines();
-        nbt.putString("damage", new TranslationTextComponent("infooverlay.damage").appendText("" + HitUtils.getDamage(this.baseDamage, this.world, this.pos.offset(this.getFacing())).getValue()).getFormattedText());
+    public CompoundNBT getInfo() {
+        CompoundNBT nbt = super.getInfo();
+        nbt.putString("damage", new TranslationTextComponent("infooverlay.hit.damage").appendText(String.valueOf(this.dmg)).getFormattedText());
+        if (this.attackOnlyAdults) {
+            nbt.putString("onlyAdults", new TranslationTextComponent("infooverlay.hit.onlyadults").getFormattedText());
+        }
         return nbt;
     }
 }
--- src/main/java/com/lazynessmind/blockactions/actions/placeaction/PlacerTileEntity.java
+++ src/main/java/com/lazynessmind/blockactions/actions/placeaction/PlacerTileEntity.java
@@ -1,16 +1,20 @@
 package com.lazynessmind.blockactions.actions.placeaction;
 
-import com.lazynessmind.blockactions.base.BlockActionTileEntityBase;
+import com.lazynessmind.blockactions.base.BlockActionTileEntity;
 import com.lazynessmind.blockactions.event.TileEntityRegister;
 import com.lazynessmind.blockactions.utils.InvUtils;
+import com.lazynessmind.blockactions.utils.InvUtils.InvObject;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class PlacerTileEntity extends BlockActionTileEntityBase implements ITickableTileEntity {
+import javax.annotation.Nonnull;
+
+public class PlacerTileEntity extends BlockActionTileEntity implements ITickableTileEntity {
 
     public Direction direction;
 
@@ -26,32 +30,40 @@ public class PlacerTileEntity extends BlockActionTileEntityBase implements ITick
                     this.direction = this.getBlockState().get(PlacerBlock.FACING);
                 }
 
-                if (!this.world.isBlockPowered(this.pos)) {
-                    InvUtils.InvObject invObject = PlacerUtils.getBlockStateFromInvAbove(this.pos, this.world);
-                    BlockState fromInv = invObject.getState();
+                InvObject invItem = PlacerUtils.getBlockStateFromInvAbove(this.pos, this.world);
+                BlockState fromInv = invItem.getState();
 
-                    //Stop working is the current block to place is null or the block in front isn't air.
-                    if (fromInv != Blocks.AIR.getDefaultState() && this.world.getBlockState(pos.offset(this.getFacing())) == Blocks.AIR.getDefaultState()) {
-                        this.workTime--;
-                        this.markDirty();
-                    }
+                if (fromInv != Blocks.AIR.getDefaultState() && this.world.getBlockState(pos.offset(this.getFacing())) == Blocks.AIR.getDefaultState()) {
+                    this.decreaseWorkTime();
+                }
 
+                if (this.isEnergyMode()) {
+                    this.energyManager.canInsert(true);
                     if (this.canWork()) {
-                        if (this.getFacing() != null) {
-                            if (fromInv != Blocks.AIR.getDefaultState()) {
-                                BlockPos facingPos = this.pos.offset(this.getFacing());
-                                if (this.world.isAirBlock(facingPos)) {
-                                    this.world.setBlockState(facingPos, fromInv, 11);
-                                    this.world.playSound(null, pos, this.world.getBlockState(facingPos).getSoundType().getPlaceSound(), SoundCategory.BLOCKS, 1f, 1f);
-                                    this.setWorkTime(this.getCoolDown());
-                                    this.markDirty();
-                                    if (invObject.getState() != Blocks.AIR.getDefaultState()) {
-                                        InvUtils.removeFromInvAbove(this.pos, this.world, invObject.getIndex());
-                                    }
-                                }
-                            }
-                        }
+                        this.doWork(this.world, this.pos, true);
                     }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void doWork(@Nonnull World world, BlockPos pos, boolean energyMode) {
+        InvObject invItem = PlacerUtils.getBlockStateFromInvAbove(this.pos, this.world);
+        BlockState fromInv = invItem.getState();
+
+        if (this.getFacing() != null) {
+            if(this.isEnergyMode() && !this.energyManager.canRemove(this.getEnergyCost())) return;
+            if (fromInv != Blocks.AIR.getDefaultState()) {
+                BlockPos facingPos = this.pos.offset(this.getFacing());
+                if (this.world.isAirBlock(facingPos)) {
+                    this.world.setBlockState(facingPos, fromInv, 11);
+                    this.world.playSound(null, pos, this.world.getBlockState(facingPos).getSoundType().getPlaceSound(), SoundCategory.BLOCKS, 1f, 1f);
+                    this.setWorkTime(this.getCoolDown());
+                    if (invItem.getState() != Blocks.AIR.getDefaultState()) {
+                        InvUtils.removeFromInvAbove(this.pos, this.world, invItem.getIndex());
+                    }
+                    if (energyMode) this.energyManager.removeEnergy(this.getEnergyCost());
                 }
             }
         }
